5191d04ead65798b5a070115004097c389eee888
Josiah Bruner <jsbruner@umich.edu>
2018-08-15 15:15:06 -0400

    Add build scripts and start defining fields for full primary

--- build.sh
+++ build.sh
@@ -2,3 +2,26 @@
 #
 # Build script for cuptane. Gets dependencies, builds tuf, and then builds
 # cuptane implementation and tests.
+
+# Build TUF.
+
+function buildtuf {
+  cd rust-tuf;
+  cargo build;
+  cd ..;
+}
+
+
+# Ask to build TUF
+echo "Do you want to build TUF?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) buildtuf; break;;
+        No ) break;;
+    esac
+done
+
+# Build Uptane source libs
+cd src;
+
+gcc -c -ansi primary-full.c -o primary-full.o # Builds full primary
