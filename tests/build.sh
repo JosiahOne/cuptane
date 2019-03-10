262fe34f0a05f27eb7482caa2b16ff781460b606
Josiah Bruner <jsbruner@umich.edu>
2019-03-10 18:58:24 -0400

    Configure build options for test program:

---
+++ tests/build.sh
@@ -0,0 +1,5 @@
+# This script just redirects to the main build script in the root of cuptane.
+
+cd ../;
+./build.sh;
+cd src;
---
+++ tests/full-client-updater.c
@@ -0,0 +1,15 @@
+/* Full Client Update Client
+ *
+ *   This is an example update client for a "full primary".
+ *   It takes the path to an update package as the single argument
+ */
+
+/* Preliminaries:
+ *   A private key already exists on the ECU at priv.key
+ */
+
+int main(int argc, char *argv[])
+{
+ 
+  return 0;
+}
